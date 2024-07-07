6ea87adf703d9d32752434fe5296e527e1495aae
dighion <142634850+dighion@users.noreply.github.com>
2024-07-07 20:21:01 -0300

    add pytest

--- train.py
+++ train.py
@@ -22,46 +22,60 @@ def reset_seeds():
    np.random.seed(42)
    random.seed(42)
 
-data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+def read_data():
+    data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+    X=data.drop(["fetal_health"], axis=1)
+    y=data["fetal_health"]
+    return X, y
 
-X=data.drop(["fetal_health"], axis=1)
-y=data["fetal_health"]
+def process_data(X, y):
+    columns_names = list(X.columns)
+    scaler = preprocessing.StandardScaler()
+    X_df = scaler.fit_transform(X)
+    X_df = pd.DataFrame(X_df, columns=columns_names)
 
-columns_names = list(X.columns)
-scaler = preprocessing.StandardScaler()
-X_df = scaler.fit_transform(X)
-X_df = pd.DataFrame(X_df, columns=columns_names)
-
-X_train, X_test, y_train, y_test = train_test_split(X_df,
+    X_train, X_test, y_train, y_test = train_test_split(X_df,
                                                     y,
                                                     test_size=0.3,
                                                     random_state=42)
 
-y_train = y_train -1
-y_test = y_test - 1
+    y_train = y_train -1
+    y_test = y_test - 1
+    return X_train, X_test, y_train, y_test
 
-reset_seeds()
-model = Sequential()
-model.add(InputLayer(input_shape=(X_train.shape[1], )))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(3, activation='softmax'))
+def create_model(X):
+    reset_seeds()
+    model = Sequential()
+    model.add(InputLayer(input_shape=(X.shape[1], )))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
 
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'diego.ghion'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = 'a2819fb018cedfe45cbc0fc9e630aedb10440648'
-mlflow.set_tracking_uri('https://dagshub.com/diego.ghion/mlops-ead.mlflow')
+def config_mlflow():
+    os.environ['MLFLOW_TRACKING_USERNAME'] = 'diego.ghion'
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = 'a2819fb018cedfe45cbc0fc9e630aedb10440648'
+    mlflow.set_tracking_uri('https://dagshub.com/diego.ghion/mlops-ead.mlflow')
 
-mlflow.tensorflow.autolog(log_models=True,
-                          log_input_examples=True,
-                          log_model_signatures=True)
+    mlflow.tensorflow.autolog(log_models=True,
+                              log_input_examples=True,
+                              log_model_signatures=True)
 
-with mlflow.start_run(run_name='experiment_mlops_ead') as run:
-  model.fit(X_train,
-            y_train,
-            epochs=50,
-            validation_split=0.2,
-            verbose=3)
+def train_model(model, X_train, y_train, is_train=False):
+    with mlflow.start_run(run_name='experiment_mlops_ead') as run:
+      model.fit(X_train,
+                y_train,
+                epochs=50,
+                validation_split=0.2,
+                verbose=3)
+
+if __name__ == "__main__":
+    X, y = read_data()
+    X_train, X_test, y_train, y_test = process_data(X, y)
+    model = create_model(X)
+    config_mlflow()
+    train_model(model, X_train, y_train)
